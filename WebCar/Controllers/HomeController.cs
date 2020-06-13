63f773911b74e270f7c132a3f4db8f9ede8e3769
Loc <phuocloc123vn1999@gmail.com>
2020-06-13 09:20:39 +0700

    aa

--- WebCar/Controllers/HomeController.cs
+++ WebCar/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace WebCar.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page ya.";
 
             return View();
         }
