1077b3b4e7cd77b92958705719ba7a71fca3ea5c
Administrator <Administrator@GEKR8E0IRJ31DKI>
2020-06-13 09:18:37 +0700

    Add project files.

---
+++ WebCar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebCar.Startup))]
+namespace WebCar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
